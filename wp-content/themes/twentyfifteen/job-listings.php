e7f3fdac8782b3d9b6fffc45c6b92520b2154477
Hollyw00d <matt@mattjennings.net>
2015-10-30 10:10:32 -0700

    configured posts to appear on jobs-listings page

--- wp-content/themes/twentyfifteen/job-listings.php
+++ wp-content/themes/twentyfifteen/job-listings.php
@@ -20,6 +20,36 @@ get_header(); ?>
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
 
+			<?php
+			$args = array(
+				'post_type'      => 'job',
+				'orderby'        => 'title',
+				'order'          => 'asc',
+			);
+
+			$lib_query = new WP_Query(array('post_type' => 'job', 'order' => 'asc', 'posts_per_page' => 3));
+			if ($lib_query->have_posts()) {
+				while ($lib_query->have_posts()) : $lib_query->the_post();
+
+					$img_src = wp_get_attachment_image_src(get_post_thumbnail_id($post->ID));
+
+					?>
+					<div class="single-job">
+
+						<p><strong><a href="<?php the_permalink(); ?>">_job_title:</strong> <?php echo get_post_meta($post->ID, '_job_title', true); ?></a></p>
+
+						<p><strong>_job_salary:</strong> <?php echo get_post_meta($post->ID, '_job_salary', true); ?></p>
+
+						<p><strong>Single Image:</strong><br /><?php echo get_the_post_thumbnail(); ?></p>
+
+						<p><strong>Background Image:</strong></p>
+						<p style="background: url(<?php echo $img_src[0]; ?>); height: 100px;"></p>
+
+					</div>
+				<?php endwhile;
+				wp_reset_postdata();
+			}
+			?>
 
 
 
