e6dc9381bdce2d8946c60d1957faa808ef615ad8
Hollyw00d <matt@mattjennings.net>
2015-10-30 12:11:42 -0700

    restored older version of wp-job-listing.php that includes a single input tag in meta box

--- wp-content/plugins/wp-job-listing/wp-job-listing.php
+++ wp-content/plugins/wp-job-listing/wp-job-listing.php
@@ -8,17 +8,14 @@
  * Version: 0.0.1
  * License: GPLv2
  */
-
 // Exit if accessed directly
 if(!defined('ABSPATH')) {
     exit;
 }
-
 // Create a custom post type
 function dwwp_register_post_type() {
     $singular = 'Job Listing';
     $plural = $singular . 's';
-
     $labels = array(
         'name'                  => $plural,
         'singular_name'         => $singular,
@@ -34,7 +31,6 @@ function dwwp_register_post_type() {
         'not_found'             => 'No ' . $plural . ' found',
         'not_found_in_trash'    => 'No ' . $plural . ' in Trash'
     );
-
     $args = array(
         'labels'                => $labels,
         'public'                => true,
@@ -65,91 +61,56 @@ function dwwp_register_post_type() {
             'thumbnail'
         )
     );
-
     register_post_type('job', $args);
 }
 add_action('init', 'dwwp_register_post_type');
-
-
 /**
  * Adds a box to the main column on the Post and Page edit screens.
  */
 function job_listings_add_meta_box() {
-
-        add_meta_box(
-            'jobs_listings_id',
-            __( 'Job', 'job_listings_plugin' ),
-            'jobs_listings_meta_box_callback',
-            // Slug that matches the custom post type above
-            'jobs'
-        );
-
+    add_meta_box(
+        'jobs_listings_id',
+        __( 'Job Title', 'job_listings_plugin' ),
+        'jobs_listings_meta_box_callback',
+        // Slug that matches the custom post type above
+        'job'
+    );
 }
-
 function jobs_listings_meta_box_callback($post) {
-
-
     // Add a nonce field so we can check for it later.
     wp_nonce_field( 'myplugin_save_meta_box_data', 'myplugin_meta_box_nonce' );
-
-    $job_title_value = get_post_meta($post->ID, '_job_title', true);
-    $job_salary_value = get_post_meta($post->ID, '_job_salary', true);
-
-
-    echo 'JOB TITLE INPUT <p><label for="job_title">Title</label> <input type="text" name="job_title" id="' . $post->ID . '" value="' . $job_title_value . '" style="width: 100%;" /></p>';
-
-    echo 'JOB SALARY INPUT <p><label for="job_salary">Salary</label> <input type="text" name="job_salary" id="' . $post->ID . '" value="' . $job_salary_value . '" style="width: 100%;" /></p>';
-
-
+    $value = get_post_meta($post->ID, '_job_title', true);
+    echo '<input type="text" name="job_title" id="' . $post->ID . '" value="' . $value . '" />';
 }
-
 add_action( 'add_meta_boxes', 'job_listings_add_meta_box' );
-
 function jobs_save_meta_box_data( $post_id ) {
-
     /*
      * We need to verify this came from our screen and with proper authorization,
      * because the save_post action can be triggered at other times.
      */
-
     // Check if our nonce is set.
     if ( ! isset( $_POST['myplugin_meta_box_nonce'] ) ) {
         return;
     }
-
     // Verify that the nonce is valid.
     if ( ! wp_verify_nonce( $_POST['myplugin_meta_box_nonce'], 'myplugin_save_meta_box_data' ) ) {
         return;
     }
-
     // If this is an autosave, our form has not been submitted, so we don't want to do anything.
     if ( defined( 'DOING_AUTOSAVE' ) && DOING_AUTOSAVE ) {
         return;
     }
-
-
     if ( ! current_user_can( 'edit_post', $post_id ) ) {
         return;
     }
-
     /* OK, it's safe for us to save the data now. */
-
     // Make sure that it is set.
-    if ( !isset( $_POST['job_title']) || !isset( $_POST['job_salary'])) {
+    if ( ! isset( $_POST['job_title'] ) ) {
         return;
     }
-
     // Sanitize user input.
-    $my_data_job_title = sanitize_text_field( $_POST['job_title'] );
-    $my_data_job_salary = sanitize_text_field( $_POST['job_salary'] );
-
-
+    $my_data = sanitize_text_field( $_POST['job_title'] );
     // Update the meta field in the database.
-    update_post_meta( $post_id, '_job_title', $my_data_job_title );
-    update_post_meta( $post_id, '_job_salary', $my_data_job_salary );
-
+    update_post_meta( $post_id, '_job_title', $my_data );
 }
 add_action( 'save_post', 'jobs_save_meta_box_data' );
-
-
-
